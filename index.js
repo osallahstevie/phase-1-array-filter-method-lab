175c917df3849d83be7f79737f0fec54a797574e
osallahstevie <osallahstevie@gmail.com>
2022-11-26 23:30:25 +0300

    Finished array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+function findMatching(array, name){
+    return array.filter (
+        function (key){
+            if (key.toUpperCase()===name.toUpperCase()){
+            return true;
+            }
+        }
+    )
+}
+
+function fuzzyMatch(array, name){
+    return array.filter(
+        function (key){
+            if (key.slice(0, name.length)===name){
+            return true;
+            }
+        }
+    )
+}
+
+function matchName (array, name){
+    return array.filter (
+        function (key){
+            if (key.name===name){
+                return true;
+            }
+        }  
+    )
+}
+
